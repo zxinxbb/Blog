fc8e6f176d7c96352916d49f842a5c29aaa68611
jamezz6 <92213021+jamezz6@users.noreply.github.com>
2023-09-19 14:23:21 +0100

    Update items2.js

--- backend/routes/items2.js
+++ backend/routes/items2.js
@@ -5,9 +5,9 @@ const itemsController = require('../controllers/items')
 // routes to controllers
 // the '/items' route will run the getTodos controller
 // which will fetch the data from your database
-router.get('/', itemsController.getBlogs)
-router.get('/:id', itemsController.getBlog)
-router.post('/item', itemsController.createBlog)
-router.patch('/:id', itemsController.editBlog)
-router.delete('/:id', itemsController.deleteBlog)
+router.get('/', itemsController.getUsers)
+router.get('/:id', itemsController.getUser)
+router.post('/item', itemsController.createUser)
+router.patch('/:id', itemsController.editUser)
+router.delete('/:id', itemsController.deleteUser)
 module.exports = router
