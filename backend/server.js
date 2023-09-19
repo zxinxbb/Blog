18145434ebd460f771b46672686a1d403a64f26c
AlexanderBeard1997 <139777499+AlexanderBeard1997@users.noreply.github.com>
2023-09-19 09:58:54 +0100

    backend skeleton

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+
+const express = require('express')
+const itemRoutes = require('./routes/items')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json()) // parse incomming data
+// routes for requests will always now have '/todos'
+// eg http://localhost:4000/todos/items
+app.use('/todos', itemRoutes)
+
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+    app.listen(4000, () => {
+        console.log(`listening on port 4000`)
+    })
+
+    })
+    .catch((error) => {
+        console.log(error)
+})
+// LISTEN ON PORT 4000
+// frontend is running on port 3000
+
