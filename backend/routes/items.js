7198a16641ec88d0a1922c0aff749035a8218e82
zxinxbb <zainab1309@hotmail.com>
2023-09-21 10:02:34 +0100

    fixed edit

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -6,8 +6,8 @@ const itemsController = require('../controllers/items')
 // the '/items' route will run the getTodos controller
 // which will fetch the data from your database
 router.get('/items', itemsController.getBlogs)
-router.get('/:id', itemsController.getBlog)
+router.get('/item/:id', itemsController.getBlog)
 router.post('/item', itemsController.createBlog)
-router.patch('/:id', itemsController.editBlog)
+router.patch('/item/:id', itemsController.editBlog)
 router.delete('/item/:id', itemsController.deleteBlog)
 module.exports = router
