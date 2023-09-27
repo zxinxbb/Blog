31b270e6643508686c7ee3aa9201a1a30f4e50f1
AlexanderBeard1997 <139777499+AlexanderBeard1997@users.noreply.github.com>
2023-09-27 14:05:30 +0100

    sign in Add blog

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,37 +2,35 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    
-    const {loginWithRedirect,logout, user, isLoading,} = useAuth0()
+    const { loginWithRedirect, logout, user, isLoading } = useAuth0();
+
     return (
         <div>
             <header>
-                <div> 
-                    <Link   style={{textDecoration: 'none'}}to='/'>
+                <div>
+                    <Link style={{ textDecoration: 'none' }} to='/'>
                         Homepage
                     </Link>
-                    <Link   style={{textDecoration: 'none'}}to='/add-blog'>
-                        Add Blog
-                    </Link>
+                    {user && (
+                        <Link style={{ textDecoration: 'none' }} to='/add-blog'>
+                            Add Blog
+                        </Link>
+                    )}
 
                     {!isLoading && !user && (
-                    <button
-                    onClick={() => loginWithRedirect()}
-                    >
-                    log in
-                    </button>
-    )}
+                        <button onClick={() => loginWithRedirect()}>
+                            log in
+                        </button>
+                    )}
                     {!isLoading && user && (
-                    <button
-                    onClick={() => logout()}
-                    >
-                    log out
-                    </button>
-    )}
+                        <button onClick={() => logout()}>
+                            log out
+                        </button>
+                    )}
                 </div>
             </header>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
