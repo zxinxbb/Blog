0b972e91ca437862ffa364fa418661079753d00a
AlexanderBeard1997 <139777499+AlexanderBeard1997@users.noreply.github.com>
2023-09-28 12:58:17 +0100

    css

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,48 +1,30 @@
-import { Link } from "react-router-dom"
-import { useAuth0 } from '@auth0/auth0-react'
+import { Link } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Card = ({ blog, deleteHandler }) => {
-
   const newDate = blog.date.substring(0, 10);
+  const { isAuthenticated, user } = useAuth0(); // Get user information and authentication status
 
-  const newDate = blog.date.substring(0, 10)
-  const { isAuthenticated, user } = useAuth0()
-
-
-  const isUserBlog = isAuthenticated && blog.email === user.email
-
+  // Check if the blog's email matches the user's email
+  const isUserBlog = isAuthenticated && blog.email === user.email;
 
   return (
     <div className="card">
       <h3>{blog.title}</h3>
-      <img src={blog.image} alt={blog.title} />
       <p>{blog.text}</p>
-
-
-      <div className="btn-container">
-        <button className="delete-btn" onClick={() => deleteHandler(blog)}>Delete</button>
-        <Link className="edit-btn" to={`/${blog._id}`}>Edit Blog</Link>
-      </div>
-      <div className="app-footer">
-        <footer>{newDate}</footer>
-      </div>
-    </div>
-  )}
-
-
       <img src={blog.image} alt={blog.title} width="25%" />
-      <div className="app-footer">
-      <footer>{newDate} {blog.username}</footer>
-      </div>
-      {isUserBlog && ( 
+      <footer>{newDate}</footer>
+
+      {isUserBlog && ( // Conditionally render buttons if it's the user's blog
         <>
+        <div className="btn-container">
           <button className="delete-btn" onClick={() => deleteHandler(blog)}>Delete</button>
           <Link className="edit-btn" to={`/${blog._id}`}>Edit Blog</Link>
+          </div>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-
-export default Card
+export default Card;
